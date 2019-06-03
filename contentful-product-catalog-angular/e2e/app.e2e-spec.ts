fdaa0ac21abb6709e3537f43724e97d0bef9efcb
windsolace <eugene.nwm@gmail.com>
2019-06-03 18:00:57 +0800

    Initial commit - tutorial

---
+++ contentful-product-catalog-angular/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('contentful-product-catalog-angular App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
