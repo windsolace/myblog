bd3278fbdb7b74c94a13dc7e2a1b8afb2ccf9461
windsolace <eugene.nwm@gmail.com>
2019-06-06 13:09:05 +0800

    - Added basic navigation

--- contentful-myblog/src/app/app.module.ts
+++ contentful-myblog/src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes }   from '@angular/router';
 
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContentfulService } from './contentful.service';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+import { PageAboutComponent } from './page-about/page-about.component';
 
 // define the available routes
 const routes: Routes = [
-	{ path: '', redirectTo: '/articles', pathMatch: 'full' },
+	{ path: '', component: PageHomeComponent },
+	{ path: 'about', component: PageAboutComponent },
 	{ path: 'articles', component: ArticleListComponent }
 ];
 
@@ -19,11 +22,17 @@ const routes: Routes = [
 	declarations: [
 		AppComponent,
 		ArticleListComponent,
-		ArticleComponent
+		ArticleComponent,
+		PageHomeComponent,
+		PageAboutComponent
 	],
 	imports: [
 		BrowserModule,
-		NgbCarouselModule
+		RouterModule.forRoot(
+			routes,
+			{ enableTracing: true }
+		),
+		NgbModule
 	],
 	providers: [
 		ContentfulService
