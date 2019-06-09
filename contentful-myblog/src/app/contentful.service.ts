f13d221b5b4650b8b96ff3dc95b4c1e8894edb45
windsolace <eugene.nwm@gmail.com>
2019-06-10 05:37:42 +0800

    Fix wrong import of environment file

--- contentful-myblog/src/app/contentful.service.ts
+++ contentful-myblog/src/app/contentful.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { createClient, Entry } from 'contentful';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from '../../src/environments/environment';
 
 
 const CONFIG = {
-	space: environment.contentful.space,
-	accessToken: environment.contentful.accessToken,
 
 	contentTypeIds: {
 		blogPost: '2PqfXUJwE8qSYKuM0U6w8M'
@@ -16,8 +14,8 @@ const CONFIG = {
 export class ContentfulService {
 
 	private cdaClient = createClient({
-		space: CONFIG.space,
-		accessToken: CONFIG.accessToken
+		space: environment.contentful.space, 
+		accessToken: environment.contentful.accessToken
 	});
 
 	constructor() { }
