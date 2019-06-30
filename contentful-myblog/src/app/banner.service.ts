23564f74a83825b00bb9caa0ee234ece5a6355cb
windsolace <eugene.nwm@gmail.com>
2019-06-30 21:21:05 +0800

    Refine content on desktop and large screens - 70% width rather than 100% - Article detail banner messages

--- contentful-myblog/src/app/banner.service.ts
+++ contentful-myblog/src/app/banner.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root'
 })
 export class BannerService {
-	bannerText:string[] = ["Amsterdam.", "Full Service.", "Webdevelopment.", "Wij zijn Occhio!"];
+	bannerText:string[] = [". . ."];
 	run:boolean = true;
 
 	constructor() { }
--- contentful-myblog/src/app/page-article-detail/page-article-detail.component.ts
+++ contentful-myblog/src/app/page-article-detail/page-article-detail.component.ts
@@ -3,6 +3,7 @@ import { switchMap } from 'rxjs/operators';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { ArticleService } from '../services/article.service';
 import { Article } from '../models/article';
+import { BannerService } from '../banner.service';
 
 @Component({
 	selector: 'app-page-article-detail',
@@ -13,24 +14,31 @@ export class PageArticleDetailComponent implements OnInit {
 	article: Article;
 	articleId: string = "";
 	loading: boolean = true;
+	bannerTextArr: string[] = [
+		"(^０^)ノ",
+	];
 
 	constructor(
 		private route: ActivatedRoute,
 		private router: Router,
-		private articleSvc: ArticleService
-	) { }
+		private articleSvc: ArticleService,
+		private bannerSvc:BannerService
+	) { this.bannerSvc.setText(this.bannerTextArr); }
 
 	ngOnInit() {
+		this.bannerSvc.setText(this.bannerTextArr);
 		this.articleId = this.route.snapshot.paramMap.get('id');
 		this.articleSvc.getArticle(this.articleId).subscribe(article => {
 			this.article = article;
+			this.bannerTextArr.push(this.article.title);
+			this.bannerSvc.setText(this.bannerTextArr);
 			this.loading = false;
 		});
 		
 	}
 
 	backToParent() {
-		this.router.navigate(['/articles']);
+		this.router.navigate(['/']);
 	}
 
 }
